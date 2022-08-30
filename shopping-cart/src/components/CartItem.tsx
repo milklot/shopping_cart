3df243e795b13f0ff926291f1bfcdb69163d9afb
milklot <vbiostflex@gmail.com>
2022-08-30 19:43:45 -0400

    add cart items, sidebar checkout, store page

---
+++ shopping-cart/src/components/CartItem.tsx
@@ -0,0 +1,45 @@
+import { Button, CarouselItemProps, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../contexts/ShoppingCartContext";
+import storeItems from "../data/monitors.json";
+import { formatCurrency } from "../utils/formatCurrency";
+
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+export function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find((i) => i.id === id);
+  if (item == null) return null;
+
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex aling-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+      </div>
+      <div className="text-mute" style={{ fontSize: ".75rem" }}>
+        {formatCurrency(item.price)}
+      </div>
+      <div>{formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
+    </Stack>
+  );
+}
--- shopping-cart/src/pages/Store.tsx
+++ shopping-cart/src/pages/Store.tsx
@@ -1,3 +1,17 @@
+import { Row, Col } from "react-bootstrap";
+import monitors from "../data/monitors.json";
+import { StoreItem } from "../components/StoreItem";
+
 export function Store() {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {monitors.map((monitor) => (
+          <Col key={monitor.id}>
+            <StoreItem {...monitor} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
